00d177bbb760e084e086a19ecccd17eacd78e75a
Dev-Steven <stevenchung650@gmail.com>
2024-07-08 11:28:35 -0700

    Initial Commit

---
+++ SubmitForm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubmitForm
+//
+//  Created by Steven Chung on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubmitForm/SubmitFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmitFormApp.swift
+//  SubmitForm
+//
+//  Created by Steven Chung on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SubmitFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
